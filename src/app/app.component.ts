7d66d4bcc9ad9b30f3790974b5ee24c0831abb7a
Sid130698 <sidsingh130698@gmail.com>
2024-10-06 23:55:16 +0530

    revising angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'data-binding';
+  firstName = "Siddharth Singh"
+  imageThumbsUp = "assets/images/thumbs-up.png";
+  imageScene = "../assets/images/scene.jpg";
+  passValueToComponent(e:any){
+    console.log("Value  ", e.target.value);
+  }
 }
